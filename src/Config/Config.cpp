35b418127f59f90078ac6b130ac912551c426bc1
zsd2024 <347225+xcon@user.noreply.gitee.com>
2025-06-29 22:06:41 +0800

    优化设置提示窗口

--- src/Config/Config.cpp
+++ src/Config/Config.cpp
@@ -1,31 +1,32 @@
 #include "Config.h"
 
-Config::Config() {
+Config::Config()
+{
 	QDir configDir = QDir(QCoreApplication::applicationDirPath());
 	settings = new QSettings(configDir.filePath("config.ini"), QSettings::IniFormat);
 }
 
-Config::~Config() {
+Config::~Config()
+{
 	delete settings;
 }
 
-bool Config::getAutoPunch() {
+bool Config::getAutoPunch()
+{
 	if (!settings->contains(QAnyStringView(QString("tools/auto_punch"))))
 	{
-		QMessageBox::StandardButton reply = QMessageBox::question(nullptr, "配置设置", "是否开启自动打卡？", QMessageBox::Yes | QMessageBox::No, QMessageBox::Yes);
+		QMessageBox msgBox(QMessageBox::Question, "配置设置", "是否开启自动打卡？", QMessageBox::Yes | QMessageBox::No, nullptr);
+		msgBox.setWindowFlags(Qt::Dialog | Qt::CustomizeWindowHint | Qt::WindowTitleHint);
+		QMessageBox::StandardButton reply = static_cast<QMessageBox::StandardButton>(msgBox.exec());
 		if (reply == QMessageBox::Yes)
-		{
 			setAutoPunch(true);
-		}
 		else
-		{
 			setAutoPunch(false);
-		}
 	}
 	return settings->value(QAnyStringView(QString("tools/auto_punch"))).toBool();
-	
 }
 
-void Config::setAutoPunch(bool value) {
+void Config::setAutoPunch(bool value)
+{
 	settings->setValue(QAnyStringView(QString("tools/auto_punch")), value);
 }
