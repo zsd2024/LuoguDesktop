c0b46be631d4b84a4e517d0c9030e33da03bbdc5
zsd2024 <347225+xcon@user.noreply.gitee.com>
2025-10-15 22:13:12 +0800

    添加翻译

---
+++ translations/en_US.ts
@@ -0,0 +1,342 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>Config</name>
+    <message>
+        <location filename="../src/Config/Config.cpp" line="20"/>
+        <source>是否开启自动打卡？</source>
+        <translation>Enable automatic check-in?</translation>
+    </message>
+    <message>
+        <location filename="../src/Config/Config.cpp" line="22"/>
+        <source>是（&amp;Y）</source>
+        <translation>&amp;Yes</translation>
+    </message>
+    <message>
+        <location filename="../src/Config/Config.cpp" line="23"/>
+        <source>否（&amp;N）</source>
+        <translation>&amp;No</translation>
+    </message>
+</context>
+<context>
+    <name>Error</name>
+    <message>
+        <location filename="../src/Error/Error.cpp" line="15"/>
+        <source>很抱歉，发生了内部错误。</source>
+        <translation>We apologize, an internal error has occurred.</translation>
+    </message>
+    <message>
+        <location filename="../src/Error/Error.cpp" line="22"/>
+        <source>错误信息：</source>
+        <translation>Error Message:</translation>
+    </message>
+    <message>
+        <location filename="../src/Error/Error.cpp" line="22"/>
+        <source>错误文件：</source>
+        <translation>Error File:</translation>
+    </message>
+    <message>
+        <location filename="../src/Error/Error.cpp" line="22"/>
+        <source>错误行号：</source>
+        <translation>Error Line:</translation>
+    </message>
+    <message>
+        <location filename="../src/Error/Error.cpp" line="22"/>
+        <source>错误函数：</source>
+        <translation>Error Function:</translation>
+    </message>
+</context>
+<context>
+    <name>LoginWindow</name>
+    <message>
+        <location filename="../src/Login/Login.ui" line="26"/>
+        <location filename="../src/Login/ui_Login.h" line="525"/>
+        <source>登录 - LuoguDesktop</source>
+        <translation>Login - LuoguDesktop</translation>
+    </message>
+    <message>
+        <location filename="../src/Login/Login.ui" line="194"/>
+        <location filename="../src/Login/ui_Login.h" line="527"/>
+        <source>在洛谷，享受 Coding 的欢乐！</source>
+        <translation>On Luogu, enjoy the joy of Coding!</translation>
+    </message>
+    <message>
+        <location filename="../src/Login/Login.ui" line="423"/>
+        <location filename="../src/Login/ui_Login.h" line="528"/>
+        <source>用户名</source>
+        <translation>Username</translation>
+    </message>
+    <message>
+        <location filename="../src/Login/Login.ui" line="441"/>
+        <location filename="../src/Login/ui_Login.h" line="530"/>
+        <source>用户名、手机号或电子邮箱</source>
+        <translation>Username, phone number, or email address</translation>
+    </message>
+    <message>
+        <location filename="../src/Login/Login.ui" line="662"/>
+        <location filename="../src/Login/Login.ui" line="683"/>
+        <location filename="../src/Login/ui_Login.h" line="531"/>
+        <location filename="../src/Login/ui_Login.h" line="533"/>
+        <source>密码</source>
+        <translation>Password</translation>
+    </message>
+    <message>
+        <location filename="../src/Login/Login.ui" line="904"/>
+        <location filename="../src/Login/ui_Login.h" line="534"/>
+        <source>验证码</source>
+        <translation>CAPTCHA</translation>
+    </message>
+    <message>
+        <location filename="../src/Login/Login.ui" line="927"/>
+        <location filename="../src/Login/ui_Login.h" line="536"/>
+        <source>右侧图形验证码</source>
+        <translation>Right-side CAPTCHA</translation>
+    </message>
+    <message>
+        <location filename="../src/Login/Login.ui" line="976"/>
+        <location filename="../src/Login/ui_Login.h" line="538"/>
+        <source>登录</source>
+        <translation>Login</translation>
+    </message>
+    <message>
+        <location filename="../src/Login/Login.ui" line="1198"/>
+        <location filename="../src/Login/ui_Login.h" line="539"/>
+        <source>忘记密码/两步验证</source>
+        <translation>Forget Password/Two-Factor Authentication</translation>
+    </message>
+    <message>
+        <location filename="../src/Login/Login.ui" line="1346"/>
+        <location filename="../src/Login/ui_Login.h" line="540"/>
+        <source>第三方平台登录</source>
+        <translation>Third-Party Platform Login</translation>
+    </message>
+    <message>
+        <location filename="../src/Login/Login.ui" line="1482"/>
+        <location filename="../src/Login/ui_Login.h" line="543"/>
+        <source>没有账号？</source>
+        <translation>Don’t have an account?</translation>
+    </message>
+    <message>
+        <location filename="../src/Login/Login.ui" line="1500"/>
+        <location filename="../src/Login/ui_Login.h" line="544"/>
+        <source>注册</source>
+        <translation>Sign Up</translation>
+    </message>
+    <message>
+        <location filename="../src/Login/Login.cpp" line="40"/>
+        <source>登录未完成，确定关闭？</source>
+        <translation>Close without finishing login?</translation>
+    </message>
+    <message>
+        <location filename="../src/Login/Login.cpp" line="41"/>
+        <source>关闭确认</source>
+        <translation>Confirm Close</translation>
+    </message>
+    <message>
+        <location filename="../src/Login/Login.cpp" line="63"/>
+        <source>登录失败</source>
+        <translation>Login Failed</translation>
+    </message>
+    <message>
+        <location filename="../src/Login/Login.cpp" line="76"/>
+        <source>欢迎回来，%0！</source>
+        <translation>Welcome back, %0!</translation>
+    </message>
+    <message>
+        <location filename="../src/Login/Login.cpp" line="77"/>
+        <source>登录成功</source>
+        <translation>Login Successful</translation>
+    </message>
+</context>
+<context>
+    <name>LuoguDesktop</name>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="14"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="129"/>
+        <source>LuoguDesktop</source>
+        <translation>LuoguDesktop</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="32"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="144"/>
+        <source>文件（&amp;F）</source>
+        <translation>&amp;File</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="40"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="145"/>
+        <source>题目（&amp;P）</source>
+        <translation>&amp;Problem</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="46"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="146"/>
+        <source>讨论（&amp;D）</source>
+        <translation>&amp;Discuss</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="57"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="147"/>
+        <source>帮助（&amp;H）</source>
+        <translation>&amp;Help</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="64"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="148"/>
+        <source>专栏（&amp;A）</source>
+        <translation>&amp;Article</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="77"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="130"/>
+        <source>登录</source>
+        <translation>Login</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="82"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="131"/>
+        <source>登出</source>
+        <translation>Logout</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="87"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="132"/>
+        <source>设置</source>
+        <translation>Settings</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="92"/>
+        <location filename="../src/LuoguDesktop.cpp" line="72"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="133"/>
+        <source>退出</source>
+        <translation>Quit</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="97"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="134"/>
+        <source>帮助</source>
+        <translation>Help</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="102"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="135"/>
+        <source>关于</source>
+        <translation>About</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="107"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="136"/>
+        <source>查看题目</source>
+        <translation>Show Problem</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="112"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="137"/>
+        <source>我的专栏</source>
+        <translation>My Articles</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="117"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="138"/>
+        <source>文章广场</source>
+        <translation>Articles Hub</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="122"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="139"/>
+        <source>全部板块</source>
+        <translation>All Sections</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="127"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="140"/>
+        <source>站务版</source>
+        <translation>Siteaffairs Section</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="132"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="141"/>
+        <source>题目总版</source>
+        <translation>Problem Section</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="137"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="142"/>
+        <source>学术版</source>
+        <translation>Academics Section</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="142"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="143"/>
+        <source>保存帖子</source>
+        <translation>Save Discuss</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.cpp" line="80"/>
+        <source>登出失败</source>
+        <translation>Logout failed</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.cpp" line="79"/>
+        <source>登出失败，是否直接关闭 LuoguDesktop？</source>
+        <translation>Logout failed. Close LuoguDesktop anyway?</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.cpp" line="81"/>
+        <source>是（&amp;Y）</source>
+        <translation>&amp;Yes</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.cpp" line="82"/>
+        <source>否（&amp;N）</source>
+        <translation>&amp;No</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.cpp" line="92"/>
+        <source>显示</source>
+        <translation>Show</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.cpp" line="189"/>
+        <source>欢迎使用 LuoguDesktop！</source>
+        <translation>Welcome to LuoguDesktop!</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.cpp" line="203"/>
+        <source>累计通过题目</source>
+        <translation>Total Solved Problems</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../src/LuoguDesktop.cpp" line="213"/>
+        <source>%n&lt;span style=&apos;font-size:10px;&apos;&gt;道&lt;/span&gt;</source>
+        <translation>
+            <numerusform>%n&lt;span style=&quot;font-size:10px;&quot;&gt; problem&lt;/span&gt;</numerusform>
+            <numerusform>%n&lt;span style=&quot;font-size:10px;&quot;&gt; problems&lt;/span&gt;</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.cpp" line="225"/>
+        <source>累计参加 Rated 比赛</source>
+        <translation>Rated Contests Participated</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../src/LuoguDesktop.cpp" line="233"/>
+        <source>%n&lt;span style=&apos;font-size:10px;&apos;&gt;场&lt;/span&gt;</source>
+        <translation>
+            <numerusform>%n&lt;span style=&quot;font-size:10px;&quot;&gt;  contest&lt;/span&gt;</numerusform>
+            <numerusform>%n&lt;span style=&quot;font-size:10px;&quot;&gt;  contests&lt;/span&gt;</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.cpp" line="348"/>
+        <source>关于 LuoguDesktop</source>
+        <translation>About LuoguDesktop</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.cpp" line="348"/>
+        <source>&lt;b&gt;版本：&lt;/b&gt;0.1 测试版&lt;br&gt;&lt;b&gt;开发者：&lt;/b&gt;zsd2024&lt;br&gt;&lt;b&gt;GitHub：&lt;/b&gt;&lt;a href=&apos;https://github.com/zsd2024/LuoguDesktop&apos;&gt;zsd2024/LuoguDesktop&lt;/a&gt;&lt;center&gt;&lt;font color=&apos;#808080&apos;&gt;Copyright (C) 2025 zsd2024. &lt;/font&gt;&lt;/center&gt;</source>
+        <translation>&lt;b&gt;Version:&lt;/b&gt; 0.1 Test Version&lt;br&gt;&lt;b&gt;Developer:&lt;/b&gt; zsd2024&lt;br&gt;&lt;b&gt;GitHub:&lt;/b&gt;&lt;a href=&apos;https://github.com/zsd2024/LuoguDesktop&apos;&gt;zsd2024/LuoguDesktop&lt;/a&gt;&lt;center&gt;&lt;font color=&apos;#808080&apos;&gt;Copyright (C) 2025 zsd2024. &lt;/font&gt;&lt;/center&gt;</translation>
+    </message>
+</context>
+</TS>
---
+++ translations/zh_CN.ts
@@ -0,0 +1,340 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>Config</name>
+    <message>
+        <location filename="../src/Config/Config.cpp" line="20"/>
+        <source>是否开启自动打卡？</source>
+        <translation>是否开启自动打卡？</translation>
+    </message>
+    <message>
+        <location filename="../src/Config/Config.cpp" line="22"/>
+        <source>是（&amp;Y）</source>
+        <translation>是（&amp;Y）</translation>
+    </message>
+    <message>
+        <location filename="../src/Config/Config.cpp" line="23"/>
+        <source>否（&amp;N）</source>
+        <translation>否（&amp;N）</translation>
+    </message>
+</context>
+<context>
+    <name>Error</name>
+    <message>
+        <location filename="../src/Error/Error.cpp" line="15"/>
+        <source>很抱歉，发生了内部错误。</source>
+        <translation>很抱歉，发生了内部错误。</translation>
+    </message>
+    <message>
+        <location filename="../src/Error/Error.cpp" line="22"/>
+        <source>错误信息：</source>
+        <translation>错误信息：</translation>
+    </message>
+    <message>
+        <location filename="../src/Error/Error.cpp" line="22"/>
+        <source>错误文件：</source>
+        <translation>错误文件：</translation>
+    </message>
+    <message>
+        <location filename="../src/Error/Error.cpp" line="22"/>
+        <source>错误行号：</source>
+        <translation>错误行号：</translation>
+    </message>
+    <message>
+        <location filename="../src/Error/Error.cpp" line="22"/>
+        <source>错误函数：</source>
+        <translation>错误函数：</translation>
+    </message>
+</context>
+<context>
+    <name>LoginWindow</name>
+    <message>
+        <location filename="../src/Login/Login.ui" line="26"/>
+        <location filename="../src/Login/ui_Login.h" line="525"/>
+        <source>登录 - LuoguDesktop</source>
+        <translation>登录 - LuoguDesktop</translation>
+    </message>
+    <message>
+        <location filename="../src/Login/Login.ui" line="194"/>
+        <location filename="../src/Login/ui_Login.h" line="527"/>
+        <source>在洛谷，享受 Coding 的欢乐！</source>
+        <translation>在洛谷，享受 Coding 的欢乐！</translation>
+    </message>
+    <message>
+        <location filename="../src/Login/Login.ui" line="423"/>
+        <location filename="../src/Login/ui_Login.h" line="528"/>
+        <source>用户名</source>
+        <translation>用户名</translation>
+    </message>
+    <message>
+        <location filename="../src/Login/Login.ui" line="441"/>
+        <location filename="../src/Login/ui_Login.h" line="530"/>
+        <source>用户名、手机号或电子邮箱</source>
+        <translation>用户名、手机号或电子邮箱</translation>
+    </message>
+    <message>
+        <location filename="../src/Login/Login.ui" line="662"/>
+        <location filename="../src/Login/Login.ui" line="683"/>
+        <location filename="../src/Login/ui_Login.h" line="531"/>
+        <location filename="../src/Login/ui_Login.h" line="533"/>
+        <source>密码</source>
+        <translation>密码</translation>
+    </message>
+    <message>
+        <location filename="../src/Login/Login.ui" line="904"/>
+        <location filename="../src/Login/ui_Login.h" line="534"/>
+        <source>验证码</source>
+        <translation>验证码</translation>
+    </message>
+    <message>
+        <location filename="../src/Login/Login.ui" line="927"/>
+        <location filename="../src/Login/ui_Login.h" line="536"/>
+        <source>右侧图形验证码</source>
+        <translation>右侧图形验证码</translation>
+    </message>
+    <message>
+        <location filename="../src/Login/Login.ui" line="976"/>
+        <location filename="../src/Login/ui_Login.h" line="538"/>
+        <source>登录</source>
+        <translation>登录</translation>
+    </message>
+    <message>
+        <location filename="../src/Login/Login.ui" line="1198"/>
+        <location filename="../src/Login/ui_Login.h" line="539"/>
+        <source>忘记密码/两步验证</source>
+        <translation>忘记密码/两步验证</translation>
+    </message>
+    <message>
+        <location filename="../src/Login/Login.ui" line="1346"/>
+        <location filename="../src/Login/ui_Login.h" line="540"/>
+        <source>第三方平台登录</source>
+        <translation>第三方平台登录</translation>
+    </message>
+    <message>
+        <location filename="../src/Login/Login.ui" line="1482"/>
+        <location filename="../src/Login/ui_Login.h" line="543"/>
+        <source>没有账号？</source>
+        <translation>没有账号？</translation>
+    </message>
+    <message>
+        <location filename="../src/Login/Login.ui" line="1500"/>
+        <location filename="../src/Login/ui_Login.h" line="544"/>
+        <source>注册</source>
+        <translation>注册</translation>
+    </message>
+    <message>
+        <location filename="../src/Login/Login.cpp" line="40"/>
+        <source>登录未完成，确定关闭？</source>
+        <translation>登录未完成，确定关闭？</translation>
+    </message>
+    <message>
+        <location filename="../src/Login/Login.cpp" line="41"/>
+        <source>关闭确认</source>
+        <translation>关闭确认</translation>
+    </message>
+    <message>
+        <location filename="../src/Login/Login.cpp" line="63"/>
+        <source>登录失败</source>
+        <translation>登录失败</translation>
+    </message>
+    <message>
+        <location filename="../src/Login/Login.cpp" line="76"/>
+        <source>欢迎回来，%0！</source>
+        <translation>欢迎回来，%0！</translation>
+    </message>
+    <message>
+        <location filename="../src/Login/Login.cpp" line="77"/>
+        <source>登录成功</source>
+        <translation>登录成功</translation>
+    </message>
+</context>
+<context>
+    <name>LuoguDesktop</name>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="14"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="129"/>
+        <source>LuoguDesktop</source>
+        <translation>LuoguDesktop</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="32"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="144"/>
+        <source>文件（&amp;F）</source>
+        <translation>文件（&amp;F）</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="40"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="145"/>
+        <source>题目（&amp;P）</source>
+        <translation>题目（&amp;P）</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="46"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="146"/>
+        <source>讨论（&amp;D）</source>
+        <translation>讨论（&amp;D）</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="57"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="147"/>
+        <source>帮助（&amp;H）</source>
+        <translation>帮助（&amp;H）</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="64"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="148"/>
+        <source>专栏（&amp;A）</source>
+        <translation>专栏（&amp;A）</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="77"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="130"/>
+        <source>登录</source>
+        <translation>登录</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="82"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="131"/>
+        <source>登出</source>
+        <translation>登出</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="87"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="132"/>
+        <source>设置</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="92"/>
+        <location filename="../src/LuoguDesktop.cpp" line="72"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="133"/>
+        <source>退出</source>
+        <translation>退出</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="97"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="134"/>
+        <source>帮助</source>
+        <translation>帮助</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="102"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="135"/>
+        <source>关于</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="107"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="136"/>
+        <source>查看题目</source>
+        <translation>查看题目</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="112"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="137"/>
+        <source>我的专栏</source>
+        <translation>我的专栏</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="117"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="138"/>
+        <source>文章广场</source>
+        <translation>文章广场</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="122"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="139"/>
+        <source>全部板块</source>
+        <translation>全部板块</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="127"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="140"/>
+        <source>站务版</source>
+        <translation>站务版</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="132"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="141"/>
+        <source>题目总版</source>
+        <translation>题目总版</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="137"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="142"/>
+        <source>学术版</source>
+        <translation>学术版</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.ui" line="142"/>
+        <location filename="../src/ui_LuoguDesktop.h" line="143"/>
+        <source>保存帖子</source>
+        <translation>保存帖子</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.cpp" line="80"/>
+        <source>登出失败</source>
+        <translation>登出失败</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.cpp" line="79"/>
+        <source>登出失败，是否直接关闭 LuoguDesktop？</source>
+        <translation>登出失败，是否直接关闭 LuoguDesktop？</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.cpp" line="81"/>
+        <source>是（&amp;Y）</source>
+        <translation>是（&amp;Y）</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.cpp" line="82"/>
+        <source>否（&amp;N）</source>
+        <translation>否（&amp;N）</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.cpp" line="92"/>
+        <source>显示</source>
+        <translation>显示</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.cpp" line="189"/>
+        <source>欢迎使用 LuoguDesktop！</source>
+        <translation>欢迎使用 LuoguDesktop！</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.cpp" line="203"/>
+        <source>累计通过题目</source>
+        <translation>累计通过题目</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../src/LuoguDesktop.cpp" line="213"/>
+        <source>%n&lt;span style=&apos;font-size:10px;&apos;&gt;道&lt;/span&gt;</source>
+        <translation>
+            <numerusform>%n&lt;span style=&apos;font-size:10px;&apos;&gt;道&lt;/span&gt;</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.cpp" line="225"/>
+        <source>累计参加 Rated 比赛</source>
+        <translation>累计参加 Rated 比赛</translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../src/LuoguDesktop.cpp" line="233"/>
+        <source>%n&lt;span style=&apos;font-size:10px;&apos;&gt;场&lt;/span&gt;</source>
+        <translation>
+            <numerusform>%n&lt;span style=&apos;font-size:10px;&apos;&gt;场&lt;/span&gt;</numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.cpp" line="348"/>
+        <source>关于 LuoguDesktop</source>
+        <translation>关于 LuoguDesktop</translation>
+    </message>
+    <message>
+        <location filename="../src/LuoguDesktop.cpp" line="348"/>
+        <source>&lt;b&gt;版本：&lt;/b&gt;0.1 测试版&lt;br&gt;&lt;b&gt;开发者：&lt;/b&gt;zsd2024&lt;br&gt;&lt;b&gt;GitHub：&lt;/b&gt;&lt;a href=&apos;https://github.com/zsd2024/LuoguDesktop&apos;&gt;zsd2024/LuoguDesktop&lt;/a&gt;&lt;center&gt;&lt;font color=&apos;#808080&apos;&gt;Copyright (C) 2025 zsd2024. &lt;/font&gt;&lt;/center&gt;</source>
+        <translation>&lt;b&gt;版本：&lt;/b&gt;0.1 测试版&lt;br&gt;&lt;b&gt;开发者：&lt;/b&gt;zsd2024&lt;br&gt;&lt;b&gt;GitHub：&lt;/b&gt;&lt;a href=&apos;https://github.com/zsd2024/LuoguDesktop&apos;&gt;zsd2024/LuoguDesktop&lt;/a&gt;&lt;center&gt;&lt;font color=&apos;#808080&apos;&gt;Copyright (C) 2025 zsd2024. &lt;/font&gt;&lt;/center&gt;</translation>
+    </message>
+</context>
+</TS>
